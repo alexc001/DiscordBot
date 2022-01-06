eaa19a23a77cdd42423ea256288a53e926d1dc6b
Alex C <89596056+alexc001@users.noreply.github.com>
2022-01-06 08:56:08 -0500

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -2,6 +2,20 @@ module.exports = {
     name: 'skip',
     description: 'skip music command',
     async execute(client, message, args, color, options, embed, database, config, chalk, userdb, distube){
-        distube.skip(message);
+        message.delete();
+
+        const {guild, member, mentions} = message
+        const {name} = guild
+        const icon = guild.iconURL({dynamic: true})
+
+
+        if(member.roles.cache.find(r=>r.name === config.djRole) || member.hasPermission('ADMINISTRATOR')){
+            distube.skip(message);
+        } else {
+            options.permissionEmbed(embed, "no permission to skip")
+            message.channel.send(embed).then(msg=>{msg.delete({timeout: 4000})})
+            return;
+        }
+
     }
 }
