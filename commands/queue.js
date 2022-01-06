fb93684cd11dd149856605cf5eafd4cbce277441
Alex C <89596056+alexc001@users.noreply.github.com>
2022-01-06 08:54:36 -0500

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,12 +1,22 @@
+const {MessageEmbed} = require('discord.js')
 module.exports = {
     name: 'queue',
     description: 'queue music command',
     async execute(client, message, args, color, options, embed, database, config, chalk, userdb, distube){
-        let queue = distube.getQueue(message);
-        message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
-            `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
-        ).slice(0, 10).join("\n"));
-
+        let queue = distube.getQueue(message)
+        if(!queue){
+            options.errorEmbed(embed, "There is no queue")
+            message.channel.send(embed).then(msg=>{msg.delete({timeout: 5000})})
+            return
+        }
+        const queueEmbed = new MessageEmbed()
+        .setDescription('Current queue:\n' + queue.songs.map((song, id) =>
+        `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
+    ).slice(0, 10).join("\n"))
+        .setColor(color)
+        .setFooter("Music", icon)
+        .setTimestamp()
+        message.channel.send(queueEmbed)
        
     }
 }
