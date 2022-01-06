fa857b20f8f65379a26461d1c953f1dd025b5c13
Alex C <89596056+alexc001@users.noreply.github.com>
2022-01-06 08:53:58 -0500

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -2,6 +2,12 @@ module.exports = {
     name: 'play',
     description: 'play music command',
     async execute(client, message, args, color, options, embed, database, config, chalk, userdb, distube){
+        message.delete();
+        if(!args[0]){
+            options.errorEmbed(embed, 'Please mention a song name!')
+            message.channel.send(embed)
+            message.delete({timeout: 5000})
+        }
         distube.play(message, args.join(" "));
     }
 }
