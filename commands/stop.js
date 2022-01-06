5192848221b742df36e96ef13e809e97d2806dcd
Alex C <89596056+alexc001@users.noreply.github.com>
2022-01-06 08:56:43 -0500

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -2,7 +2,24 @@ module.exports = {
     name: 'stop',
     description: 'stop music command',
     async execute(client, message, args, color, options, embed, database, config, chalk, userdb, distube){
-        distube.stop(message);
-        message.channel.send("Stopped the music!");
+        message.delete();
+
+        const {guild, member, mentions} = message
+        const {name} = guild
+        const icon = guild.iconURL({dynamic: true})
+
+
+        if(member.roles.cache.find(r=>r.name === config.djRole) || member.hasPermission('ADMINISTRATOR')){
+            distube.stop(message)
+            message.channel.send("Stopped the music!").then(msg=>{msg.delete({timeout: 5000})})
+        } else {
+            options.permissionEmbed(embed, "no permission to stop the music")
+            message.channel.send(embed).then(msg=>{msg.delete({timeout: 4000})})
+            return;
+        }
+
+
+
+        
     }
 }
